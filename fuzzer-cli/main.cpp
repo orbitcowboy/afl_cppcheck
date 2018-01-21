3d2749e75a481e624ff8258b929e3a4ba749a070
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2018-01-21 12:20:18 +0100

    cppcheck-fuzzing-client: add mode '1' for generating partially valid code

--- fuzzer-cli/main.cpp
+++ fuzzer-cli/main.cpp
@@ -29,23 +29,32 @@ public:
                         const unsigned int value) {}
 };
 
-
-static const char * const data[] = {
+static const char * const data0[] = {
  "(", ")", "{", "}", "[", "]",
  "<", "<=", "==", "!=", ">=", ">",
- "+", "-", "*", "/", "%", "&", "|", "^", "&&", "||", "++", "--", "="
+ "+", "-", "*", "/", "%", "~", "&", "|", "^", "&&", "||", "++", "--", "=", "?", ":"
  "name1", "name2", "name3", "name4", "name5", "name6",
  "const", "void", "char", "int", "enum", "if", "else", "while", "for", "switch", "case", "default", "return", "continue", "break", "struct", "typedef",
- ";", ",", ":",
+ ";", ",",
  "1", "0.1", "0xff", "-5", "\"abc\"", "'x'",
 };
 
+static const char * const data1[] = {
+ "(", ")", "[", "]",
+ "<", "<=", "==", "!=", ">=", ">",
+ "+", "-", "*", "/", "%", "~", "&", "|", "^", "&&", "||", "++", "--", "=", "?", ":"
+ "name1", "name2", "name3", "name4", "name5", "name6",
+ ",",
+ "1", "0.1", "0xff", "-5", "\"abc\"", "'x'",
+};
 
+#define NUM(data)  (sizeof(data)/sizeof(*data))
 
-static void writeCode(std::ostream &ostr, unsigned int *value, unsigned int *ones, const unsigned int min) {
+static void writeCode(std::ostream &ostr, int type, unsigned int *value, unsigned int *ones, const unsigned int min) {
   static char par[20] = {' ',0};
   static char parindex;
-  const unsigned int num = sizeof(data) / sizeof(*data);
+  const unsigned int num = (type == 0) ? NUM(data0) : NUM(data1);
+  const char * const * const data = (type == 0) ? data0 : data1;
   while (*ones > min) {
       unsigned char i = *value % num;
       *value = *value / num;
@@ -71,12 +80,22 @@ static void writeCode(std::ostream &ostr, unsigned int *value, unsigned int *one
 }
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
+  const char *filename = nullptr;
+  int type = 0;
+
+  for (int i = 1; i < argc; ++i) {
+	if (strcmp(argv[i],"1")==0)
+	  type = 1;
+	else
+	  filename = argv[i];  
+  }
+
+  if (!filename) {
     std::cout << "Invalid args\n";
     return 1;
   }
+
   std::ostringstream ostr;
-  char inp[64] = {0};
   std::ifstream f(argv[1]);
   if (!f.is_open()) {
     std::cout << "fopen\n";
@@ -86,13 +105,21 @@ int main(int argc, char **argv) {
   unsigned int ones = 0;
   while (f.good()) {
     unsigned char c = f.get();
-    value = (value << 8) + c;
-    ones = (ones << 8) + 0xff;
-    writeCode(ostr, &value, &ones, 0x100);
+    value = (value << 8) | c;
+    ones = (ones << 8) | 0xff;
+    writeCode(ostr, type, &value, &ones, 0x100);
   }
-  writeCode(ostr, &value, &ones, 0);
+  writeCode(ostr, type, &value, &ones, 0);
 
-  const std::string code = ostr.str();
+  std::string code;
+  switch (type) {
+  case 0:
+    code = ostr.str();
+    break;
+  case 1:
+    code = "void f() {\n  x=" + ostr.str() + "\n}";
+    break;
+  }
   std::cout << code << std::endl;
 
   CppcheckExecutor cppcheckExecutor;
