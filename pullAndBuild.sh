fd34ae794920adf9c91483a1ffa4da8b4cf610d8
orbitcowboy <orbitcowboy@web.de>
2018-01-26 10:48:29 +0100

    pullAndBuild.sh: Instrument cppcheck with afl-g++

--- pullAndBuild.sh
+++ pullAndBuild.sh
@@ -4,27 +4,29 @@ set -e
 # This small script pulls the latest version of afl-fuzz and cppcheck
 # and builds the executables from:
 #  - afl-fuzz
-#  - cppcheck   (compiled with g++)
+#  - cppcheck   (instrumented by afl-g++)
 #  - fuzzer-cli (instrumented by afl-g++)
 
 # Determine the current working directory
 WORKINGDIR=$(dirname $(readlink -f $0))
+AFL_GPP=../afl/afl-g++
 # Update afl-sources and build
 
 cd ${WORKINGDIR}/afl
 git stash
 git pull
-make clean all -j4
+make all -j4
 
 # Update cppcheck-sources and build 
 cd ${WORKINGDIR}/cppcheck
 git stash
 git pull
-make clean all -j4
+make CXX=${AFL_GPP} all -j4
 
 # Build fuzzer-cli executable
 cd ${WORKINGDIR}/fuzzer-cli/
 make clean all
+
 
 # Return back to previous directory
 cd ${WORKINGDIR}
