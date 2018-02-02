75f255f92f4935824e3e0397fbe564f982e18060
orbitcowboy <orbitcowboy@web.de>
2018-02-02 23:27:43 +0100

    reduce.sh: Minor cleanups.

--- reduce.sh
+++ reduce.sh
@@ -7,6 +7,7 @@ set -e
 # Determine the current working directory
 WORKINGDIR=$(dirname $(readlink -f $0))
 CURRENT_DATE=$(date +'%m_%d_%Y-%H-%M-%S')
+CPPCHECK_CALL='../cppcheck/cppcheck --enable=all --inconclusive'
 rm -fR ${WORKINGDIR}/crashes
 mkdir ${WORKINGDIR}/crashes
 # copy crashes from afl-output into crashes folder
@@ -19,7 +20,7 @@ j=0; for i in id* ; do let j+=1 ; ${WORKINGDIR}/fuzzer-cli/cppcheck-fuzzing-clie
 # Cleanup files from afl-output/crashes
 rm -f id*
 # reduce with afl-tmin
-j=0; for i in *.c ; do let j+=1 ; ${WORKINGDIR}/afl/afl-tmin -t 5000 -i $i -o r$i -- ../cppcheck/cppcheck --enable=all --inconclusive @@ ; done
+j=0; for i in *.c ; do let j+=1 ; ${WORKINGDIR}/afl/afl-tmin -t 5000 -i $i -o r$i -- ${CPPCHECK_CALL} @@ ; done
 # cleanup original crashes and keep reduced
 rm crash*.c
 j=0; for i in rcrash*.c ; do let j+=1 ; mv $i crash_${CURRENT_DATE}_$j.c ; done
