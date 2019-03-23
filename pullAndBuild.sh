6644653989e88260094c22560b3be65c854ae016
orbitcowboy <orbitcowboy@web.de>
2019-03-23 15:38:03 +0100

    Cleanup pullAndBulid.sh script

--- pullAndBuild.sh
+++ pullAndBuild.sh
@@ -8,29 +8,29 @@ set -e
 #  - fuzzer-cli (instrumented by afl-clang++)
 
 # Determine the current working directory
-WORKINGDIR=$(dirname $(readlink -f $0))
+WORKINGDIR=$(dirname $(readlink -f "$0"))
 AFL_GPP=../afl/afl-clang++
 # Update afl-sources and build
 
-cd ${WORKINGDIR}/afl 
+cd "$WORKINGDIR"/afl 
 git stash
 git pull
 make clean
 make all -j12 CXX=clang++ CC=clang
 
 # Update cppcheck-sources and build 
-cd ${WORKINGDIR}/cppcheck
+cd "$WORKINGDIR"/cppcheck
 git stash
 git pull
 make clean
 make CXX=${AFL_GPP} -j12
 
 # Build fuzzer-cli executable
-cd ${WORKINGDIR}/fuzzer-cli/
+cd "$WORKINGDIR"/fuzzer-cli/
 make CXX=${AFL_GPP} CXXFLAGS="-static -O1" clean all
 
 # Return back to previous directory
-cd ${WORKINGDIR}
+cd "$WORKINGDIR"
 
 
 
