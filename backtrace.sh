72bd40bd0405127108db13c93366cd3c2836135a
orbitcowboy <orbitcowboy@web.de>
2019-03-23 15:56:49 +0100

    Script cleanup.

--- backtrace.sh
+++ backtrace.sh
@@ -6,6 +6,6 @@ set -e
 WORKINGDIR=$(dirname $(readlink -f "$0"))
 cd "$WORKINGDIR/crashes"
 # create gdb backtrace files
-j=0; for i in crash*.c*; do let j+=1; echo "${i}" && gdb -batch-silent -ex "run" -ex "set logging overwrite on" -ex "set logging file gdb.txt" -ex "set logging on" -ex "set pagination off" -ex "handle SIG33 pass nostop noprint" -ex "echo backtrace:\\n" -ex "backtrace full" -ex "echo \\n\\nregisters:\\n\\n" -ex "info registers" -ex "echo \\n\\ncurrent instructions:\\n" -ex "x/16i \$pc" -ex "echo \\n\\nthreads backtrace:\\n" -ex "thread apply all backtrace" -ex "set logging off" -ex "quit" --args "$WORKINGDIR/cppcheck/cppcheck" --enable=all --inconclusive "${i}" && mv gdb.txt "${i}"_bt.txt; done
+j=0; for i in crash*.c*; do (( j+=1 )) ; echo "${i}" && gdb -batch-silent -ex "run" -ex "set logging overwrite on" -ex "set logging file gdb.txt" -ex "set logging on" -ex "set pagination off" -ex "handle SIG33 pass nostop noprint" -ex "echo backtrace:\\n" -ex "backtrace full" -ex "echo \\n\\nregisters:\\n\\n" -ex "info registers" -ex "echo \\n\\ncurrent instructions:\\n" -ex "x/16i \$pc" -ex "echo \\n\\nthreads backtrace:\\n" -ex "thread apply all backtrace" -ex "set logging off" -ex "quit" --args "$WORKINGDIR/cppcheck/cppcheck" --enable=all --inconclusive "${i}" && mv gdb.txt "${i}"_bt.txt; done
 # go back to working directory
 cd "$WORKINGDIR"
--- reduce.sh
+++ reduce.sh
@@ -7,7 +7,6 @@ set -e
 # Determine the current working directory
 WORKINGDIR=$(dirname $(readlink -f "$0"))
 CURRENT_DATE=$(date +'%m_%d_%Y-%H-%M-%S')
-CPPCHECK_CALL='../cppcheck/cppcheck --enable=all --inconclusive'
 rm -fR "$WORKINGDIR/crashes"
 mkdir "$WORKINGDIR/crashes"
 # copy crashes from afl-output into crashes folder
@@ -16,14 +15,14 @@ cd "$WORKINGDIR/crashes"
 # cleanup potential duplicates
 fdupes -N -d -q .
 # Execute cppcheck-fuzzing-client to generate files 
-j=0; for i in id* ; do let j+=1; "$WORKINGDIR"/fuzzer-cli/cppcheck-fuzzing-client --type1 --translate-input "$i" > crash"$j".c; done
+j=0; for i in id* ; do (( j+=1 )) ; "$WORKINGDIR"/fuzzer-cli/cppcheck-fuzzing-client --type1 --translate-input "$i" > crash"$j".c; done
 # Cleanup files from afl-output/crashes
 rm -f id*
 # reduce with afl-tmin
-j=0; for i in *.c ; do let j+=1; "$WORKINGDIR/afl/afl-tmin" -t 5000 -i "$i" -o r"$i" -- ${CPPCHECK_CALL} @@ ; done
+j=0; for i in *.c ; do (( j+=1 )) ; "$WORKINGDIR/afl/afl-tmin" -t 5000 -i "$i" -o r"$i" -- "$WORKINGDIR"/cppcheck/cppcheck --enable=all --inconclusive @@ ; done
 # cleanup original crashes and keep reduced
 rm crash*.c
-j=0; for i in rcrash*.c ; do let j+=1; mv "$i" crash_${CURRENT_DATE}_"$j".c ; done
+j=0; for i in rcrash*.c ; do (( j+=1 )) ; mv "$i" crash_"$CURRENT_DATE"_"$j".c ; done
 # cleanup potential duplicates
 fdupes -N -d -q .
 # go back to working directory
