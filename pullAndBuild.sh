03802d5636d18ee8597e2e42c77ced34d1d9af58
orbitcowboy <orbitcowboy@web.de>
2019-03-25 11:55:51 +0100

    Determine the number CPU cores for pull and build.

--- pullAndBuild.sh
+++ pullAndBuild.sh
@@ -9,6 +9,7 @@ set -e
 
 # Determine the current working directory
 WORKINGDIR=$(dirname $(readlink -f "$0"))
+CPU_CORES_ON_SYSTEM=$(eval "/usr/bin/nproc")
 AFL_GPP=../afl/afl-clang++
 # Update afl-sources and build
 
@@ -16,14 +17,14 @@ cd "$WORKINGDIR"/afl
 git stash
 git pull
 make clean
-make all -j12 CXX=clang++ CC=clang
+make all -j${CPU_CORES_ON_SYSTEM} CXX=clang++ CC=clang
 
 # Update cppcheck-sources and build 
 cd "$WORKINGDIR"/cppcheck
 git stash
 git pull
 make clean
-make CXX=${AFL_GPP} -j12
+make CXX=${AFL_GPP} -j${CPU_CORES_ON_SYSTEM}
 
 # Build fuzzer-cli executable
 cd "$WORKINGDIR"/fuzzer-cli/
